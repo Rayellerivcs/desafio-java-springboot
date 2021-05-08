7623663bac233d5a65551915d7c29e4b15d3ba51
Rayellerivcs <rivcs@cin.ufpe.br>
2021-05-08 10:28:33 -0300

    Removing comments

--- src/main/java/br/com/rayelle/product/repository/ProductRepository.java
+++ src/main/java/br/com/rayelle/product/repository/ProductRepository.java
@@ -6,8 +6,4 @@ import br.com.rayelle.product.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, String> {
 
-//	@Query(value ="SELECT * FROM Product obj " +
-//		"WHERE ((:q IS NULL OR UPPER(obj.name) LIKE UPPER('%:q%') ) OR (:q IS NULL OR UPPER(obj.description) LIKE UPPER('%:q%'))) AND  (:min_price IS NULL OR obj.price >= :min_price)  AND  (:max_price IS NULL OR obj.price <=:max_price)", nativeQuery = true)
-//	List<Product> search(@Param("q") String q,@Param("min_price") Double min_price,@Param("max_price") Double max_price);
-
 }
